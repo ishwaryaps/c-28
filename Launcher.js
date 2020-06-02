aee04c4466bb7cde10e94c062f0dd03590926994
ishwaryaps <65061051+ishwaryaps@users.noreply.github.com>
2020-06-02 17:02:25 +0530

    Add files via upload

---
+++ Launcher.js
@@ -0,0 +1,29 @@
+class Launcher{
+    constructor(bodyA,pointB){
+        var options={
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.05,
+            length:5
+
+
+        }
+        this.pointB=pointB
+        this.launch=Constraint.create(options);
+        World.add(world,this.launch);
+
+    }
+    fly(){
+        this.launch.bodyA=null;
+    }
+    display(){
+        if(this.launch.bodyA)
+        {
+        var pointA=this.launch.bodyA.position;
+        var pointB=this.pointB
+        strokeWeight(4);
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+        }
+        
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,71 @@
+const Constraint=Matter.Constraint;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj, paperObject,groundObject	
+var world;
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	dustbinObj=new Dustbin(1200,650);
+	paperObject=new Paper(200,450,70);
+	groundObject=new Ground(width/2,670,width,20);
+	launcher=new Launcher(paperObject.body,{x:200,y:100});
+	//Create a Ground
+	
+
+	var render = Render.create({
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1200,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
+
+	Engine.run(engine);
+	//Render.run(render);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+ 
+  dustbinObj.display();
+  paperObject.display();
+  groundObject.display();
+  launcher.display();
+  
+ 
+  
+  
+ 
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(paperObject.body,{x:mouseX,y:mouseY})
+}
+function mouseReleased(){
+    launcher.fly();
+
+
+}
+
+//function keyPressed() {
+  	//if (keyCode === UP_ARROW) {
+
+    	//Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+    
+  	//}
+//}
